18e5262eb4b674d49b2bddb7d26e9f7fbb14dc85
Cole Mickens <cole.mickens@gmail.com>
2017-01-17 23:07:44 -0800

    wip

--- jenkins/seedjobs.groovy
+++ jenkins/seedjobs.groovy
@@ -1,11 +1,24 @@
 def prefix = "acs-jenkins-msi"
-def repo = "github.com/Azure/acs-engine"
+def repo = "https://github.com/Azure/acs-engine"
 def branch = "master"
 def locations = ["westus", "eastus"]
 
+job("acs-engine/seedjob") {
+	scm {
+		git("${repo}", branch)
+	}
+
+	steps {
+		dsl {
+			external('jenkins/seedjobs.groovy')
+			removeAction('DISABLE')
+		}
+	}
+}
+
 locations.each {
 	def location = it
-	def jobName = "${prefix}-${location}"
+	def jobName = "acs-engine/${prefix}-${location}"
 	job(jobName) {
 		scm {
 			git("${repo}", branch)
