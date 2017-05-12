3c016827e4539030f36787d5f0fdb421ac2f78a6
Cole Mickens <cole.mickens@gmail.com>
2017-05-12 03:26:13 -0700

    experimental deployer

--- cmd/generate.go
+++ cmd/generate.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/Azure/acs-engine/pkg/acsengine"
 	"github.com/Azure/acs-engine/pkg/api"
+	"github.com/Azure/acs-engine/pkg/armhelpers"
 )
 
 const (
@@ -18,6 +19,8 @@ const (
 )
 
 type generateCmd struct {
+	authArgs
+
 	apimodelPath      string
 	outputDirectory   string // can be auto-determined from clusterDefinition
 	caCertificatePath string
@@ -29,6 +32,12 @@ type generateCmd struct {
 	// derived
 	containerService *api.ContainerService
 	apiVersion       string
+
+	// experimental
+	client        armhelpers.UberClient
+	deploy        bool
+	resourceGroup string
+	location      string
 }
 
 func NewGenerateCmd() *cobra.Command {
@@ -39,7 +48,8 @@ func NewGenerateCmd() *cobra.Command {
 		Short: generateShortDescription,
 		Long:  generateLongDescription,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return gc.run(cmd, args)
+			gc.validate(cmd, args)
+			return gc.run()
 		},
 	}
 
@@ -51,6 +61,11 @@ func NewGenerateCmd() *cobra.Command {
 	f.BoolVar(&gc.classicMode, "classic-mode", false, "enable classic parameters and outputs")
 	f.BoolVar(&gc.noPrettyPrint, "no-pretty-print", false, "skip pretty printing the output")
 	f.BoolVar(&gc.parametersOnly, "parameters-only", false, "only output parameters files")
+	f.BoolVar(&gc.deploy, "deploy", false, "deploy as well")
+	f.StringVar(&gc.resourceGroup, "resource-group", "", "resource group to deploy to")
+	f.StringVar(&gc.location, "location", "", "location to deploy to")
+
+	addAuthFlags(&gc.authArgs, f)
 
 	return generateCmd
 }
@@ -93,10 +108,21 @@ func (gc *generateCmd) validate(cmd *cobra.Command, args []string) {
 
 	gc.containerService = containerService
 	gc.apiVersion = apiVersion
+
+	if gc.deploy {
+		gc.client, err = gc.authArgs.getClient()
+		if err != nil {
+			log.Fatalf("failed to get client") // TODO: cleanup
+		}
+
+		if gc.resourceGroup == "" {
+			cmd.Usage()
+			log.Fatal("--resource-group is required when deploying")
+		}
+	}
 }
 
-func (gc *generateCmd) run(cmd *cobra.Command, args []string) error {
-	gc.validate(cmd, args)
+func (gc *generateCmd) run() error {
 	log.Infoln("Generating...")
 
 	templateGenerator, err := acsengine.InitializeTemplateGenerator(gc.classicMode)
@@ -122,6 +148,12 @@ func (gc *generateCmd) run(cmd *cobra.Command, args []string) error {
 
 	if err = acsengine.WriteArtifacts(gc.containerService, gc.apiVersion, template, parameters, gc.outputDirectory, certsGenerated, gc.parametersOnly); err != nil {
 		log.Fatalf("error writing artifacts: %s \n", err.Error())
+	}
+
+	if gc.deploy {
+		log.Warnf("DEPLOY GOES HERE")
+		log.Warnf("DEPLOY GOES HERE - use location")
+		log.Warnf("DEPLOY GOES HERE")
 	}
 
 	return nil
